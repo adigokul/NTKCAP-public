97fdecfdbd5dd8a7d0d1673093a0e302f9cfa532
eddie <ed2die5@gmail.com>
2026-01-11 02:10:50 +0800

    Clean up install folder - remove redundant scripts and add .gitignore

--- install/scripts/fix_encoding.ps1
+++
@@ -1,6 +0,0 @@
-# Fix encoding for setup.ps1
-$scriptPath = "C:\Users\Nuvo-8003(EMC)\NTKCAP\install\scripts\setup.ps1"
-$content = Get-Content $scriptPath -Raw -Encoding UTF8
-$Utf8BomEncoding = New-Object System.Text.UTF8Encoding $true
-[System.IO.File]::WriteAllText($scriptPath, $content, $Utf8BomEncoding)
-Write-Host "Successfully converted setup.ps1 to UTF-8 with BOM" -ForegroundColor Green
